1820d53015b804641162dba0d3b992b42dcb175b
Steve Tran <anhtran@bl5ck.com>
2018-10-01 17:48:09 +0800

    fix config js ejecting

--- src/config.js
+++ src/config.js
@@ -1,28 +1,7 @@
-// <!-- eject:remove -->
-import config from 'config.json';
-// <!-- /eject:remove -->
 export const CACHEABLE_APIS = [];
 export const DEFAULT_API_FETCH_TIMEOUT = 10000;
-// <!-- eject:replace with='export const REST_API = ${&#39;}/${&#39;};' -->
-export const REST_API = config.apiServer;
-// <!-- /eject:replace -->
-export const THEME_CONFIG = {
-  // <!-- eject:remove -->
-  panelColor: '#f8f8f8',
-  bgColor: 'orange',
-  primaryColor: '#1890ff',
-  primaryLightColor: '#e5f7ff',
-  black: '#001529',
-  white: '#fff',
-  defaultPadding: 24,
-  // <!-- /eject:remove -->
-};
-// <!-- eject:remove if='!args.multilingual' -->
+export const REST_API = '/';
+export const THEME_CONFIG = {};
+
 export const INTL_LOCALE =
-  // <!-- eject:remove -->
-  window.indexad.INTL_LOCALE ||
-  // <!-- /eject:remove -->
-  navigator.language ||
-  navigator.browserLanguage ||
-  'en-US';
-// <!-- /eject:remove -->
+  navigator.language || navigator.browserLanguage || 'en-US';
